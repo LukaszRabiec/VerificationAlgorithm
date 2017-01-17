609d7a2528e4907831b18e409cdef388eec7beef
Lukasz Rabiec <lukaszrabiec@gmail.com>
2017-01-18 00:15:33 +0100

    Add project files.

---
+++ VerificationAlgorithm.Tests/HamiltonianCycleTests.cs
@@ -0,0 +1,70 @@
+﻿namespace VerificationAlgorithm.Tests
+{
+    using System.Collections.Generic;
+    using FluentAssertions;
+    using Xunit;
+
+    public class HamiltonianCycleTests
+    {
+        int[,] _graph = CreateGraph();
+
+        [Fact]
+        public void GraphWithHamiltonianCycle()
+        {
+            var cycle1 = new List<int> { 0, 1, 5, 4, 2, 3, 0 };
+            var cycle2 = new List<int> { 4, 5, 1, 3, 2, 0, 4 };
+
+            var isHamiltonian1 = HamiltonianCycle.Verify(_graph, cycle1);
+            var isHamiltonian2 = HamiltonianCycle.Verify(_graph, cycle2);
+
+            isHamiltonian1.Should().BeTrue();
+            isHamiltonian2.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CycleAndGraphHaveDifferentSizes()
+        {
+            var cycle1 = new List<int> { 0, 1, 2, 0 };
+            var cycle2 = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 0 };
+
+            var isHamiltonian1 = HamiltonianCycle.Verify(_graph, cycle1);
+            var isHamiltonian2 = HamiltonianCycle.Verify(_graph, cycle2);
+
+            isHamiltonian1.Should().BeFalse();
+            isHamiltonian2.Should().BeFalse();
+        }
+
+        [Fact]
+        public void SameSizesButDifferentFirstAndLastVertices()
+        {
+            var cycle = new List<int> { 0, 1, 5, 4, 2, 3, 1 };
+
+            var isHamiltonian = HamiltonianCycle.Verify(_graph, cycle);
+
+            isHamiltonian.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CycleDoesntCoverGraphConnections()
+        {
+            var cycle = new List<int> { 0, 5, 5, 4, 3, 2, 0 };
+
+            var isHamiltonian = HamiltonianCycle.Verify(_graph, cycle);
+
+            isHamiltonian.Should().BeFalse();
+        }
+
+        private static int[,] CreateGraph()
+        {
+            return new[,]
+            {
+                { 0, 1, 1, 1, 1, 0 },
+                { 1, 0, 0, 1, 0, 1 },
+                { 1, 0, 0, 1, 1, 0 },
+                { 1, 1, 1, 0, 0, 0 },
+                { 1, 0, 1, 0, 0, 1 },
+                { 0, 1, 0, 0, 1, 0 },
+            };
+        }
+    }
+}
---
+++ VerificationAlgorithm/HamiltonianCycle.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace VerificationAlgorithm
+{
+    using System.Linq;
+
+    public static class HamiltonianCycle
+    {
+        private static int _graphSize;
+        private static int _cycleSize;
+
+        public static bool Verify(int[,] graphMatrix, List<int> cycleTestimony)
+        {
+            _graphSize = graphMatrix.GetLength(0);
+            _cycleSize = cycleTestimony.Count;
+
+            if (HaveDifferentSize())
+            {
+                return false;
+            }
+
+            if (FirstAndLastCycleVerticeAreDifferent(cycleTestimony))
+            {
+                return false;
+            }
+
+            if (CycleDoesntCoverGraphConnections(graphMatrix, cycleTestimony))
+            {
+                return false;
+            }
+
+            return CycleVerticesAppearsOnlyOnce(cycleTestimony);
+        }
+
+        private static bool HaveDifferentSize()
+        {
+            return _graphSize + 1 != _cycleSize;
+        }
+
+        private static bool FirstAndLastCycleVerticeAreDifferent(List<int> cycle)
+        {
+            int firstVertice = cycle[0];
+            int lastVertice = cycle[_cycleSize - 1];
+
+            return firstVertice != lastVertice;
+        }
+
+        private static bool CycleDoesntCoverGraphConnections(int[,] graph, List<int> cycle)
+        {
+            for (int i = 1; i < _cycleSize; i++)
+            {
+                if (graph[cycle[i - 1], cycle[i]] == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool CycleVerticesAppearsOnlyOnce(List<int> cycle)
+        {
+            var counter = new int[cycle.Count - 1];
+
+            for (int vertice = 0; vertice < counter.Length; vertice++)
+            {
+                counter[vertice]++;
+            }
+
+            return counter.All(x => x == 1);
+        }
+    }
+}
